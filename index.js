a3cf070861f7c376dfd2788fffcc89b08191898f
TanveenAmbrose <racoctanveen15@gmail.com>
2025-10-19 20:59:44 +0600

    Ok doing the backend again

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const port = 3000;
 const birds = require('./routes/birds');
 app.use('/birds', birds);
 
+const loggingMiddleware = (req, res, next) => {
+    console.log('Logging done ')
+    next();
+}
+const authMiddleware = (req, res, next) => {
+    console.log('Auth done ')
+    next();
+}
+const validationMiddleware = (req, res, next) => {
+    console.log('Verification done')
+    next();
+}
+
+app.use(loggingMiddleware);
+app.use(authMiddleware);
+app.use(validationMiddleware);
+
+
 app.get('/', (req, res) => {
     res.sendFile('./dummby.html', { root: __dirname })
 })
